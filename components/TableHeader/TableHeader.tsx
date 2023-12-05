f2cd029c0e86c24482c89e44c206c5f333a546c7
andreea21ungureanu <andreea.ungureanu221@gmail.com>
2023-12-05 01:39:20 +0000

    change component directive

--- components/TableHeader/TableHeader.tsx
+++ components/TableHeader/TableHeader.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
-import { getProjectDetails } from "@/lib/getAllProjects";
+import { getProjectDetails } from "../../lib/getAllProjects";
 
 type TableHeaderProps = {
   title: string;
